92fe7278df7e25d785169255ccd7cd70eb328e89
gyj <1152527010@qq.com>
2023-04-15 19:12:00 +0800

    1

---
+++ src/com/text/student.java
@@ -0,0 +1,16 @@
+package com.text;
+
+public class student {
+    public String name;
+    public static void main(String[] args){
+        System.out.print("hello");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
