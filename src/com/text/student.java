bbcfb374422cf27ed34f539818dcfc63adb16423
gyj <1152527010@qq.com>
2023-04-15 19:17:00 +0800

    2

--- src/com/text/student.java
+++ src/com/text/student.java
@@ -2,8 +2,17 @@ package com.text;
 
 public class student {
     public String name;
+    public String id;
     public static void main(String[] args){
         System.out.print("hello");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
